323ae45516b2571c6ff10a6bf6110fa12ffde9b6
Ahmed Fouad <ahmedfouad01099@gmail.com>
2020-12-21 20:19:25 +0200

    Updates

---
+++ precache-manifest.6a37c519869529d43ebffe1c162dee6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfb4dc1858c65d41dec1",
+    "url": "/city-tours-app/static/css/main.ac5416f2.chunk.css"
+  },
+  {
+    "revision": "bfb4dc1858c65d41dec1",
+    "url": "/city-tours-app/static/js/main.bfb4dc18.chunk.js"
+  },
+  {
+    "revision": "2a748f918b639472c3ba",
+    "url": "/city-tours-app/static/js/runtime~main.2a748f91.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/city-tours-app/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "12a6659ff5467c71b599841286fb8e7f",
+    "url": "/city-tours-app/static/media/logo.12a6659f.svg"
+  },
+  {
+    "revision": "5f70f05940cd45ec081b3c3c7db83978",
+    "url": "/city-tours-app/index.html"
+  }
+];
